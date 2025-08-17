f9b1958e325eb4ab9a683b11013558f4ae7fa6aa
4ass4 <76assistant76@gmail.com>
2025-08-17 17:16:07 +0300

    'deploy'

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/quest-forge-mini-app/sw.js', { scope: '/quest-forge-mini-app/' })})}
